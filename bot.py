08213aaaf40aa1afb6be146cacb243da9551c409
Sufyan <bucks.pri@gmail.com>
2024-06-29 00:36:12 +0530

    bot fixed

--- bot.py
+++ bot.py
@@ -4,13 +4,17 @@ import os
 
 
 bot_api=os.getenv("bot_api")
-bot=telebot.TeleBot(token=bot_api)
+print(bot_api)
+bot=telebot.TeleBot(token="7289171817:AAFUvQvcS6nHSEdXys7I7aRaCsLUbB2fd4A")
 
 @bot.message_handler(commands=["start"])
-def start(message):
-    bot.reply_to(message,"Hello, Glad you texted")
+def send_welcome(message):
+    bot.reply_to(message, "Hello, Glad you texted!!")
 
+@bot.message_handler(commands=["get_my_id"])
+def get_my_id(message):
+    bot.send_message(chat_id=message.chat.id,text=f"Your Chat id is : {message.chat.id}")
 
 if __name__=="__main__":
-    print("Basic Bot is Live :)")
+    print("Bot is Live!!")
     bot.polling()
