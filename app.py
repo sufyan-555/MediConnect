6c930271e72df9ba4a7c2a378a17e9bbc4e7fbcc
Navya <navyac.social@gmail.com>
2024-06-29 02:27:18 +0530

    app.py updated

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ app=Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route('/login_page')
+def login_page():
+    return render_template("login.html")
+
+@app.route('/signup_page')
+def signup_page():
+    return render_template("signup.html")
+
+@app.route('/login',methods=["POST"])
+def login():
+    return render_template("dash.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
