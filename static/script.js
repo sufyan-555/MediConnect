7161355c204b4e742d5ca92b219eb0a794e1affc
Pratik Singh <pratikkumars623@gmail.com>
2024-06-29 00:25:38 +0530

    FAQ section added

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// Mobile navbar
 const btn=document.getElementById('menu-btn')
 const nav=document.getElementById('menu')
 
@@ -8,3 +9,19 @@ btn.addEventListener('click',()=>
     nav.classList.toggle('hidden')
 })
 
+
+// Javascript for FAQ section
+
+const dtElements=document.querySelectorAll('dt')
+dtElements.forEach(element=>{
+    element.addEventListener('click',()=>
+    {
+        const ddId=element.getAttribute('aria-controls');
+        const ddElement=document.getElementById(ddId);
+        const ddArrowIcon=element.querySelectorAll('i')[0];
+
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+    })
+})
